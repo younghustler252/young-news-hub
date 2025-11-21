052b2b89d6219876f7a38c4fd48fc3631fe49524
Young tech <younghustler252@email.com>
2025-11-21 16:50:52 +0100

    Fix case-sensitive filename for Netlify

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,14 +2,12 @@
 import React from "react";
 import TopBar from "./header/TopBar";
 import MainHeader from "./header/MainHeader";
-import NavBar from "./header/NavBar";
 
 const Header = () => {
   return (
     <header className="relative z-50">
       <TopBar />
       <MainHeader />
-      {/* <NavBar /> */}
     </header>
   );
 };
